4bd3b4d7f45f16b4d43bd7be0e63038ef56a3804
mvanamen <mvanamen@xebia.com>
2023-03-09 15:16:32 +0100

    COMMIT COMMIT

---
+++ testactions/az-remote-backend-variables.tf
@@ -0,0 +1,18 @@
+# company
+variable "company" {
+  type = string
+  description = "This variable defines the name of the company"
+  default = "XSECpipelinetest"
+}
+# environment
+variable "environment" {
+  type = string
+  description = "This variable defines the environment to be built"
+  default = "prod"
+}
+# azure region
+variable "location" {
+  type = string
+  description = "Azure region where resources will be created"
+  default = "West Europe"
+}
