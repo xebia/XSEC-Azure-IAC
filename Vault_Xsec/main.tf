bbddbabadb6d5b44825a6e449abdee11d3849fbc
mvanamen <mvanamen@xebia.com>
2023-03-10 09:10:24 +0100

    updating the directories

---
+++ Vault_Xsec/main.tf
@@ -0,0 +1,32 @@
+resource "azurerm_resource_group" "res-0" {
+  location = "westeurope"
+  name     = "Vault_Xsec"
+}
+resource "azurerm_kubernetes_cluster" "res-1" {
+  dns_prefix          = "xsecvaultaks1"
+  location            = "westeurope"
+  name                = "Xsec"
+  resource_group_name = "Vault_Xsec"
+  tags = {
+    Environment = "Production"
+  }
+  default_node_pool {
+    name    = "default"
+    vm_size = "Standard_D2_v2"
+  }
+  identity {
+    type = "SystemAssigned"
+  }
+  depends_on = [
+    azurerm_resource_group.res-0,
+  ]
+}
+resource "azurerm_kubernetes_cluster_node_pool" "res-2" {
+  kubernetes_cluster_id = "/subscriptions/1a0d078e-b0e6-432d-89c7-8a75cac664aa/resourceGroups/Vault_Xsec/providers/Microsoft.ContainerService/managedClusters/Xsec"
+  mode                  = "System"
+  name                  = "default"
+  vm_size               = "Standard_D2_v2"
+  depends_on = [
+    azurerm_kubernetes_cluster.res-1,
+  ]
+}
