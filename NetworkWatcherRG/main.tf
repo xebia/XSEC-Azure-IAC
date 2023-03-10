a4551630dba9c27fb24112fd91ae67d13fa7dac7
mvanamen <mvanamen@xebia.com>
2023-03-10 14:26:47 +0100

    Update main.tf

--- NetworkWatcherRG/main.tf
+++ NetworkWatcherRG/main.tf
@@ -26,19 +26,3 @@ resource "azurerm_network_watcher" "res-3" {
     azurerm_resource_group.res-0,
   ]
 }
-resource "azurerm_network_watcher_flow_log" "res-4" {
-  # checkov:skip=CKV_AZURE_12: This issue is being waivered for now
-  enabled                   = true
-  name                      = "xsecBeast-nsg-maikel-playground_asml-flowlog"
-  network_security_group_id = "/subscriptions/1a0d078e-b0e6-432d-89c7-8a75cac664aa/resourceGroups/Maikel-Playground_ASML/providers/Microsoft.Network/networkSecurityGroups/xsecBeast-nsg"
-  network_watcher_name      = "NetworkWatcher_westeurope"
-  resource_group_name       = "NetworkWatcherRG"
-  storage_account_id        = "/subscriptions/1a0d078e-b0e6-432d-89c7-8a75cac664aa/resourceGroups/prisma-cloud-resources/providers/Microsoft.Storage/storageAccounts/nsfflowlogsforprisma"
-  retention_policy {
-    days    = 30
-    enabled = true
-  }
-  depends_on = [
-    azurerm_network_watcher.res-2,
-  ]
-}
