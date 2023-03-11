9118271f97d769eaa6128f89e655a2a50d47650a
mvanamen <mvanamen@xebia.com>
2023-03-11 17:47:59 +0100

    test

--- testactions/az-remote-backend-main.tf
+++ testactions/az-remote-backend-main.tf
@@ -1,64 +1,64 @@
 # Generate a random storage name
 resource "random_string" "tf-name" {
-  length = 8
-  upper = false
+  length  = 8
+  upper   = false
   numeric = true
-  lower = true
+  lower   = true
   special = false
 }
 
 resource "random_string" "tf-name2" {
-  length = 5
-  upper = false
+  length  = 5
+  upper   = false
   numeric = false
-  lower = true
+  lower   = true
   special = false
 }
 # Create a Resource Group for the Terraform State File
 resource "azurerm_resource_group" "state-rg" {
-  name = "${lower(var.company)}-tfstate-rg"
+  name     = "${lower(var.company)}-tfstate-rg"
   location = local.location
-  
+
   lifecycle {
     prevent_destroy = true
-  }  
+  }
   tags = {
     environment = var.environment
   }
 }
 # Create a Storage Account for the Terraform State File
 resource "azurerm_storage_account" "state-sta" {
-	# checkov:skip=CKV_AZURE_59: test11
-	# checkov:skip=CKV_AZURE_59: test12
-	# checkov:skip=CKV_AZURE_33: For Testing Reasons, waivering risk
-	# checkov:skip=CKV2_AZURE_1: For Testing Reasons, waivering risk
-	# checkov:skip=CKV2_AZURE_18: For Testing Reasons, waivering risk
-	# checkov:skip=CKV_AZURE_44: For Testing Reasons, waivering risk
+  # checkov:skip=CKV_AZURE_59: test11
+  # checkov:skip=CKV_AZURE_59: test12
+  # checkov:skip=CKV_AZURE_33: For Testing Reasons, waivering risk
+  # checkov:skip=CKV2_AZURE_1: For Testing Reasons, waivering risk
+  # checkov:skip=CKV2_AZURE_18: For Testing Reasons, waivering risk
+  # checkov:skip=CKV_AZURE_44: For Testing Reasons, waivering risk
   # checkov:skip=CKV_AZURE_190: test2
   # checkov:skip=CKV_AZURE_206: test2
-  depends_on = [azurerm_resource_group.state-rg]  
-  name = "${lower(var.company)}tf${random_string.tf-name2.result}"
-  resource_group_name = azurerm_resource_group.state-rg.name
-  location = azurerm_resource_group.state-rg.location
-  account_kind = "StorageV2"
-  account_tier = "Standard"
-  access_tier = "Hot"
-  account_replication_type = "ZRS"
+  depends_on                = [azurerm_resource_group.state-rg]
+  name                      = "${lower(var.company)}tfxsecxebiastate"
+  resource_group_name       = azurerm_resource_group.state-rg.name
+  location                  = azurerm_resource_group.state-rg.location
+  account_kind              = "StorageV2"
+  account_tier              = "Standard"
+  access_tier               = "Hot"
+  account_replication_type  = "ZRS"
   enable_https_traffic_only = true
-   
+
   lifecycle {
     prevent_destroy = true
-  }  
-  
+  }
+
   tags = {
     environment = var.environment
   }
 }
 # Create a Storage Container for the Core State File
 resource "azurerm_storage_container" "core-container" {
-	# checkov:skip=CKV2_AZURE_21: For Testing Reasons, waivering risk
-  depends_on = [azurerm_storage_account.state-sta]  
-  name = "core-tfstate"
+  # checkov:skip=CKV2_AZURE_21: For Testing Reasons, waivering risk
+  depends_on           = [azurerm_storage_account.state-sta]
+  name                 = "core-tfstate"
   storage_account_name = azurerm_storage_account.state-sta.name
 }
 
