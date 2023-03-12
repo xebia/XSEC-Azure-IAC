89c1623ebcd7e863705e2e4a12f2b2dcaa7f6f6f
mvanamen <mvanamen@xebia.com>
2023-03-12 13:33:11 +0100

    Update providers.tf

--- testactions/providers.tf
+++ testactions/providers.tf
@@ -1,10 +1,10 @@
 terraform {
-  backend "azurerm" {
-    resource_group_name  = "github-actions-tfstate-rg"
-    storage_account_name = "githubactiiontfstate8154"
-    container_name       = "tfstate"
-    key                  = "nCZw6/N5EXUTeP3WzcT9kPnyg1r7gbc4i7sq8gHgfLQ7hwUiw5LI8iF+r1Np3uNK8Bg/r9rVBHWd+AStnS0u3w=="
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "github-actions-tfstate-rg"
+  #   storage_account_name = "githubactiiontfstate8154"
+  #   container_name       = "tfstate"
+  #   key                  = "nCZw6/N5EXUTeP3WzcT9kPnyg1r7gbc4i7sq8gHgfLQ7hwUiw5LI8iF+r1Np3uNK8Bg/r9rVBHWd+AStnS0u3w=="
+  # }
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
