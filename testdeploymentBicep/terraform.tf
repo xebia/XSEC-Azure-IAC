b5c9dc675cfeeb94923b324895343d243b6137ee
mvanamen <mvanamen@xebia.com>
2023-03-12 16:01:17 +0100

    Update terraform.tf

--- testdeploymentBicep/terraform.tf
+++ testdeploymentBicep/terraform.tf
@@ -1,8 +1,13 @@
 terraform {
-  backend "local" {}
+  backend "azurerm" {
+    resource_group_name  = "rg-ts-state-xsec"
+    storage_account_name = "saxsectf"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
+  }
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
       version = "3.46.0"
     }
   }
