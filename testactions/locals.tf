f3a71a8f64129884240ba8bbcdb425c34edc5725
mvanamen <mvanamen@xebia.com>
2023-03-12 16:31:14 +0100

    fully provider state done now

--- testactions/locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  #Vault Locals
-  vault_resource_group_name = "testactions"
-  location                  = "westeurope"
-}
--- testactions/main.tf
+++
@@ -1,4 +0,0 @@
-resource "azurerm_resource_group" "hashicorp_vault_xsec_resource_group" {
-  name     = local.vault_resource_group_name
-  location = local.location
-}
