5c0dfe6fe4ea59daf9e959a02c3a53a37daf9b69
mvanamen <mvanamen@xebia.com>
2023-03-12 13:31:02 +0100

    sfasdf

--- MC_Vault_Xsec_Xsec_westeurope/provider.tf
+++ MC_Vault_Xsec_Xsec_westeurope/provider.tf
@@ -1,18 +1,4 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "xsecpipelinetest-tfstate-rg"
-    storage_account_name = "xsecpipelinetesttfvjcib"
-    container_name       = "core-tfstate"
-    key                  = "TuJGoNcS8xKVGmfc17haiNCDeY1U2z4Mhf9zVYEss555b09k2qa9f2DOPb2YjQc2jqAZhNaZk+Oc+AStEEalQA=="
-  }
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-  }
-}
-
 provider "azurerm" {
-  features {}
+  features {
+  }
 }
