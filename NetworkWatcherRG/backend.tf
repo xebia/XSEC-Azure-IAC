fefc7d4a7f9a0b0a7a9b4240164c9eecd22d117e
mvanamen <mvanamen@xebia.com>
2023-03-13 15:28:18 +0100

    asdf

--- NetworkWatcherRG/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "rg-ts-state-xsec"
-    storage_account_name = "saxsectf"
-    container_name       = "terraform-state"
-    key                  = "test.terraform.tfstate"
-  }
-}
