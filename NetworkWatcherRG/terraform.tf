4a2361a074cc587a66352272beee42f249f5c69d
mvanamen <mvanamen@xebia.com>
2023-03-13 16:12:32 +0100

    psuhpushpush

--- NetworkWatcherRG/terraform.tf
+++ NetworkWatcherRG/terraform.tf
@@ -3,7 +3,7 @@ terraform {
     resource_group_name  = "rg-ts-state-xsec"
     storage_account_name = "saxsectf"
     container_name       = "terraform-state"
-    key                  = "NetworkWatcherRG.terraform.tfstate"
+    key                  = "terraform.tfstate"
   }
   required_providers {
     azurerm = {
